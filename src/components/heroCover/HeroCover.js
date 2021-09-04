205d6d2cc07241eb3168bda0cdd3bd20b0e1d662
Horacinis <horacinis@gmail.com>
2021-09-05 02:30:43 +1000

    Added and configured the navigation tabs bar

--- src/components/heroCover/HeroCover.js
+++ src/components/heroCover/HeroCover.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 
 const HeroCover = () => {
   return (
     <div>
       <h1>HOME PAGE</h1>
-      <p><Link to='/products/phones'>Phones</Link></p>
-      <p><Link to='/products/laptops'>Laptops</Link></p>
+      {/* <p><Link to='/products/phones'>Phones</Link></p> */}
+      {/* <p><Link to='/products/laptops'>Laptops</Link></p> */}
     </div>
   )
 }
--- src/components/nav/navigationBar/NavigationBar.js
+++ src/components/nav/navigationBar/NavigationBar.js
@@ -6,6 +6,7 @@ import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
+import { Link } from 'react-router-dom';
 
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
@@ -54,12 +55,12 @@ const NavigationBar = ({ products }) => {
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
+    console.log(newValue)
   };
 
   return (
     <div className={classes.root}>
       <AppBar position="static" color="default">
-
         <Tabs
           value={value}
           onChange={handleChange}
@@ -69,38 +70,19 @@ const NavigationBar = ({ products }) => {
           scrollButtons="auto"
           aria-label="scrollable auto tabs example"
         >
-
-          <Tab label="laptops" {...a11yProps(0)} />
-          <Tab label="phones" {...a11yProps(1)} />
-          <Tab label="Item Three" {...a11yProps(2)} />
-          <Tab label="Item Four" {...a11yProps(3)} />
-          <Tab label="Item Five" {...a11yProps(4)} />
-          <Tab label="Item Six" {...a11yProps(5)} />
-          <Tab label="Item Seven" {...a11yProps(6)} />
+          {products.map((product, index) => (
+            <Tab key={index} label={product} component={Link} to={`/products/${product}`} {...a11yProps({ index })} />
+          ))}
+          <Tab label="test" {...a11yProps(0)} />
+          <Tab label="test" {...a11yProps(1)} />
+          <Tab label="test" {...a11yProps(2)} />
+          <Tab label="test" {...a11yProps(3)} />
+          <Tab label="test" {...a11yProps(4)} />
+          <Tab label="test" {...a11yProps(5)} />
+          <Tab label="test" {...a11yProps(6)} />
         </Tabs>
       </AppBar>
-      {/* <TabPanel value={value} index={0}>
-        Item One
-      </TabPanel>
-      <TabPanel value={value} index={1}>
-        Item Two
-      </TabPanel>
-      <TabPanel value={value} index={2}>
-        Item Three
-      </TabPanel>
-      <TabPanel value={value} index={3}>
-        Item Four
-      </TabPanel>
-      <TabPanel value={value} index={4}>
-        Item Five
-      </TabPanel>
-      <TabPanel value={value} index={5}>
-        Item Six
-      </TabPanel>
-      <TabPanel value={value} index={6}>
-        Item Seven
-      </TabPanel> */}
-    </div>
+    </div >
   );
 }
 

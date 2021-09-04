4bb899a36f82c8dc985b0728f54a478984868c0b
Horacinis <horacinis@gmail.com>
2021-09-04 16:57:18 +1000

    added a bit of styling to the topbar component

---
+++ src/components/nav/NavbarComponent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import TopBar from './topBar/TopBar';
+
+const NavbarComponent = () => {
+  return (
+    <React.Fragment>
+      <TopBar />
+    </React.Fragment>
+  )
+}
+
+export default NavbarComponent;

12f9e8de1eedb0997145578f97b9bb9e84d051ca
Horacinis <horacinis@gmail.com>
2021-09-05 18:34:25 +1000

    Styled the products page a little

--- src/components/productItems/ProductItemsList.js
+++ src/components/productItems/ProductItemsList.js
@@ -1,8 +1,26 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import ProductItem from './ProductItem';
+import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core/styles';
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    height: 140,
+    width: 100,
+  },
+  control: {
+    padding: theme.spacing(2),
+  },
+}));
 
 const ProductItemsList = ({ product }) => {
+
+const classes = useStyles();
   let URL;
 
   switch (process.env.REACT_APP_NODE_ENV) {
@@ -40,11 +58,11 @@ const ProductItemsList = ({ product }) => {
     <div>
       <h1>{product}</h1>
       <p><Link to='/'>Home</Link></p>
-      <ul>
+      <Grid container className={classes.root} spacing={2}>
         {productItems && productItems.map(product => (
-          <li key={product._id}><ProductItem product={product} /></li>
+          <Grid item key={product._id} xs={6} sm={4} lg={3}><ProductItem product={product} /></Grid>
         ))}
-      </ul>
+      </Grid>
     </div>
   )
 }

dfd2bacf490c5c0e1568c39dc05a8b6112bacce7
Horacinis <horacinis@gmail.com>
2021-09-08 11:49:13 +1000

    Changed the value that is added to the cart quantity to integer

--- src/components/cart/ShoppingCart.js
+++ src/components/cart/ShoppingCart.js
@@ -10,7 +10,7 @@ const ShoppingCart = ({ cartItems, setCartItems }) => {
 
   const increaseDecreaseQuantity = (quantity, productItem) => {
     let newCart = [...cartItems];
-    newCart.find(product => product._id === productItem._id).quantity = quantity;
+    newCart.find(product => product._id === productItem._id).quantity = parseInt(quantity) || 1;
 
     setCartItems(newCart)
   }

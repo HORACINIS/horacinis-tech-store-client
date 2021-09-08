8ca3228b0a589563b3bf39fb0a1f2f3b9aad1843
Horacinis <horacinis@gmail.com>
2021-09-08 21:27:12 +1000

    Created the LoadingProducts component placeholder

---
+++ src/components/loading/LoadingProducts.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LoadingProducts = () => {
+  return (
+    <div>
+      <h1>Loading...</h1>
+    </div>
+  )
+}
+
+export default LoadingProducts
--- src/components/productItems/ProductItemsList.js
+++ src/components/productItems/ProductItemsList.js
@@ -1,26 +1,32 @@
 import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import ProductItem from './ProductItem';
+import LoadingProducts from './../loading/LoadingProducts';
 import Grid from '@material-ui/core/Grid';
 
 const ProductItemsList = ({ fetchedProducts, fetchProductsFunc, productCategory, addToCartFunc, getSingleProductFunc }) => {
+  // const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     fetchProductsFunc(productCategory)
   }, []); //eslint-disable-line react-hooks/exhaustive-deps
 
-  return (
-    <div>
-      <p><Link to='/'>Home</Link></p>
-      <Grid container justifyContent="center" spacing={2}>
-        {fetchedProducts && fetchedProducts.map(product => (
-          <Grid item display='flex' key={product._id} xs={6} sm={4} lg={3}>
-            <ProductItem product={product} addToCartFunc={addToCartFunc} />
-          </Grid>
-        ))}
-      </Grid>
-    </div>
-  )
+  if (fetchedProducts.length === 0) {
+    return <LoadingProducts />
+  } else {
+    return (
+      <div>
+        <p><Link to='/'>Home</Link></p>
+        <Grid container justifyContent="center" spacing={2}>
+          {fetchedProducts && fetchedProducts.map(product => (
+            <Grid item display='flex' key={product._id} xs={6} sm={4} lg={3}>
+              <ProductItem product={product} addToCartFunc={addToCartFunc} />
+            </Grid>
+          ))}
+        </Grid>
+      </div>
+    )
+  }
 }
 
 export default ProductItemsList;

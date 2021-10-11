ce70324633ca3b7dbc9c4074965695dba0010d78
Horacinis Morancinis <horacinis@gmail.com>
2021-10-11 19:01:44 +1100

    Changed the image stepper size from 400 to 350 px

--- src/components/imageStepper/imageStepperStyles.js
+++ src/components/imageStepper/imageStepperStyles.js
@@ -2,11 +2,11 @@ import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    maxWidth: 400,
+    maxWidth: 350,
     flexGrow: 1,
   },
   img: {
-    maxWidth: 400,
+    maxWidth: 350,
     overflow: 'hidden',
     display: 'block',
     width: '100%',
